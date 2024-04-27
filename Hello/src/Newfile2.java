c287588d6e85ac9e6fc3278fbaea211e789a5de4
sdac-6 <sdac-6@sdac-6-PC>
2024-04-27 10:19:31 -0700

    new file 2 created

---
+++ Hello/src/Newfile2.java
@@ -0,0 +1,14 @@
+
+
+public class Newfile2 {
+
+		public static void main(String[] args) {
+			System.out.println("new file created");
+			System.out.println("new one more line added");
+			
+		}
+
+	}
+
+
+
